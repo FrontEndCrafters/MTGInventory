99bc88f95024db145dd1c4e543f334f8343a2d52
Justin Dial <justinalandial@gmail.com>
2017-06-15 12:36:32 -0500

    Working version/added html tester

--- Server/server.py
+++ Server/server.py
@@ -1,37 +1,40 @@
 from mtgsdk import Card
-from mtgsdk import Set
-from mtgsdk import Type
-from mtgsdk import Supertype
-from mtgsdk import Subtype
-from mtgsdk import Changelog
-import urllib.request
-import os
-import subprocess
 import requests
+from flask import Flask, render_template, request, jsonify
+import socket
 import sys
-from flask import Flask, render_template, Response, request,redirect , make_response,flash,url_for,send_file
+from flask_cors import CORS
 
-ip_addr="0.0.0.0"
+ip_addr = socket.gethostbyname(socket.gethostname())
+
 
 def get_card_value(card_name):
-    return(requests.get("http://magictcgprices.appspot.com/api/cfb/price.json?cardname="+card_name).json())
+    return requests.get("http://magictcgprices.appspot.com/api/cfb/price.json?cardname="+card_name).json()
 
-app = Flask(__name__,template_folder='template')
-app.secret_key='secretsecret'
+app = Flask(__name__, template_folder='template')
+CORS(app)
+app.secret_key = 'secretsecret'
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def indexpage():
-    if request.method=='POST':
+    sys.stderr.write(request.method+'\n')
+    if request.method == 'POST':
         f = request.form
-        options=f.to_dict()
-        card = Card.where(name=get_card_url(options[0]).all())[0]
-        card_data=[card.name,card.color,get_card_value(card.name)]
-        return Response(card_data)
+        options = f.to_dict()
+        options = ''.join(options)
+        card = Card.where(name=options).all()[0]
+        card_data = {'name': str(card.name),
+                     'color': str(card.colors),
+                     'value': str(card.mana_cost),
+                     'text': str(card.text),
+                     'url': str(card.image_url)}
+        sys.stderr.write(str(card_data)+'\n')
+        return jsonify(card_data)
     
-    elif request.method=='GET':
+    elif request.method == 'GET':
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.jinja_env.cache={}
+    app.jinja_env.cache = {}
     app.run(host=ip_addr, debug=True, use_reloader=True, threaded=True)
